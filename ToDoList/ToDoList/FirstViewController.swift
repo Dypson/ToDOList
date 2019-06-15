50cb67f00a5027805fb51d3630f47ecaff17c512
Deepson Khadka <khaddeep@oisu.edu>
2019-06-15 06:36:05 -0400

    Initial Commit

---
+++ ToDoList/ToDoList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoList
+//
+//  Created by Deepson on 6/15/19.
+//  Copyright © 2019 Deepson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoList/ToDoList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by Deepson on 6/15/19.
+//  Copyright © 2019 Deepson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
